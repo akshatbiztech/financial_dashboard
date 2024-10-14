c8640ffc31a14072ff799a08ae199d633f3bf249
Akshat Ashish Shah <ak900877@dal.ca>
2024-10-14 17:53:24 -0300

    fixed build

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 import Card from "./components/Card";
 import CustomButton from "./components/CustomButton";

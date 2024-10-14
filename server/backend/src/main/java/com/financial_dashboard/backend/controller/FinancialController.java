2d8a7b1695d7e055324d6888be0e37a33f98036b
Akshat Ashish Shah <ak900877@dal.ca>
2024-10-14 06:08:00 -0300

    moved balances.json to controller dir

--- server/backend/src/main/java/com/financial_dashboard/backend/controller/FinancialController.java
+++ server/backend/src/main/java/com/financial_dashboard/backend/controller/FinancialController.java
@@ -1,6 +1,8 @@
 package com.financial_dashboard.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,8 @@ public class FinancialController {
     public Map<String, Object> getBalances() throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-        File account_data = new File("balances.json");
+        Resource resource = new ClassPathResource("balances.json");
+        File account_data = resource.getFile();
 
         Map<String, Object> balances = mapper.readValue(account_data,Map.class);
 

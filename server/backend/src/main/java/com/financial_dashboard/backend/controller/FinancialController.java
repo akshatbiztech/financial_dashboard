3cd21f3815a24f2207418d1946373e7c76f0183a
Akshat Ashish Shah <ak900877@dal.ca>
2024-10-14 01:29:46 -0300

    Backend setup and testing done- API now running

---
+++ server/backend/src/main/java/com/financial_dashboard/backend/controller/FinancialController.java
@@ -0,0 +1,24 @@
+package com.financial_dashboard.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Map;
+
+@RestController
+public class FinancialController {
+
+    @GetMapping("/balances")
+    public Map<String, Object> getBalances() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        File account_data = new File("server/backend/src/main/resources/balances.json");
+
+        Map<String, Object> balances = mapper.readValue(account_data,Map.class);
+
+        return balances;
+    }
+}
